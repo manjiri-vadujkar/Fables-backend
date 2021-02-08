502fd5419d64719743e42b588870aad53c6f3e1b
Manjiri Vadujkar <manjiri.vadujkar@gmail.com>
2021-02-08 08:04:58 +0530

    db update

--- fables.sql
+++ fables.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 07, 2021 at 02:57 PM
+-- Generation Time: Feb 08, 2021 at 03:34 AM
 -- Server version: 10.3.16-MariaDB
 -- PHP Version: 7.3.7
 
@@ -29,13 +29,13 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `book` (
-  `srno` int(11) NOT NULL,
+  `bookId` varchar(255) NOT NULL,
   `bookname` varchar(255) NOT NULL,
   `genre` varchar(255) NOT NULL,
   `author` varchar(255) NOT NULL,
   `summary` varchar(255) NOT NULL,
   `type` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -45,10 +45,10 @@ CREATE TABLE `book` (
 
 CREATE TABLE `bookchpt` (
   `srno` int(11) NOT NULL,
-  `bookname` varchar(255) NOT NULL,
+  `bookId` varchar(255) NOT NULL,
   `chptname` varchar(255) NOT NULL,
   `chptref` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -57,12 +57,12 @@ CREATE TABLE `bookchpt` (
 --
 
 CREATE TABLE `user` (
-  `userId` int(11) NOT NULL,
+  `userId` varchar(255) NOT NULL,
   `name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `subscription` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -72,9 +72,9 @@ CREATE TABLE `user` (
 
 CREATE TABLE `userfav` (
   `srno` int(11) NOT NULL,
-  `userId` int(11) NOT NULL,
-  `bookname` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `userId` varchar(255) NOT NULL,
+  `bookId` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -84,9 +84,9 @@ CREATE TABLE `userfav` (
 
 CREATE TABLE `userread` (
   `srno` int(11) NOT NULL,
-  `userId` int(11) NOT NULL,
-  `bookname` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `userId` varchar(255) NOT NULL,
+  `bookId` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -96,8 +96,8 @@ CREATE TABLE `userread` (
 
 CREATE TABLE `userrecommend` (
   `srno` int(11) NOT NULL,
-  `userId` int(11) NOT NULL,
-  `bookname` varchar(255) NOT NULL
+  `userId` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `bookId` varchar(255) CHARACTER SET utf8 NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -108,38 +108,103 @@ CREATE TABLE `userrecommend` (
 -- Indexes for table `book`
 --
 ALTER TABLE `book`
-  ADD PRIMARY KEY (`srno`);
+  ADD PRIMARY KEY (`bookId`);
 
 --
 -- Indexes for table `bookchpt`
 --
 ALTER TABLE `bookchpt`
-  ADD PRIMARY KEY (`srno`);
+  ADD PRIMARY KEY (`srno`),
+  ADD KEY `bookId` (`bookId`);
 
 --
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`userId`),
-  ADD UNIQUE KEY `name` (`name`);
+  ADD PRIMARY KEY (`userId`);
 
 --
 -- Indexes for table `userfav`
 --
 ALTER TABLE `userfav`
-  ADD PRIMARY KEY (`srno`);
+  ADD PRIMARY KEY (`srno`),
+  ADD KEY `bookId` (`bookId`),
+  ADD KEY `userId` (`userId`);
 
 --
 -- Indexes for table `userread`
 --
 ALTER TABLE `userread`
-  ADD PRIMARY KEY (`srno`);
+  ADD PRIMARY KEY (`srno`),
+  ADD KEY `bookId` (`bookId`),
+  ADD KEY `userId` (`userId`);
 
 --
 -- Indexes for table `userrecommend`
 --
 ALTER TABLE `userrecommend`
-  ADD PRIMARY KEY (`srno`);
+  ADD PRIMARY KEY (`srno`),
+  ADD KEY `bookId` (`bookId`),
+  ADD KEY `userId` (`userId`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `bookchpt`
+--
+ALTER TABLE `bookchpt`
+  MODIFY `srno` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `userfav`
+--
+ALTER TABLE `userfav`
+  MODIFY `srno` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `userread`
+--
+ALTER TABLE `userread`
+  MODIFY `srno` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `userrecommend`
+--
+ALTER TABLE `userrecommend`
+  MODIFY `srno` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `bookchpt`
+--
+ALTER TABLE `bookchpt`
+  ADD CONSTRAINT `bookchpt_ibfk_1` FOREIGN KEY (`bookId`) REFERENCES `book` (`bookId`);
+
+--
+-- Constraints for table `userfav`
+--
+ALTER TABLE `userfav`
+  ADD CONSTRAINT `userfav_ibfk_1` FOREIGN KEY (`bookId`) REFERENCES `book` (`bookId`),
+  ADD CONSTRAINT `userfav_ibfk_2` FOREIGN KEY (`userId`) REFERENCES `user` (`userId`);
+
+--
+-- Constraints for table `userread`
+--
+ALTER TABLE `userread`
+  ADD CONSTRAINT `userread_ibfk_1` FOREIGN KEY (`bookId`) REFERENCES `book` (`bookId`),
+  ADD CONSTRAINT `userread_ibfk_2` FOREIGN KEY (`userId`) REFERENCES `user` (`userId`);
+
+--
+-- Constraints for table `userrecommend`
+--
+ALTER TABLE `userrecommend`
+  ADD CONSTRAINT `userrecommend_ibfk_1` FOREIGN KEY (`bookId`) REFERENCES `book` (`bookId`),
+  ADD CONSTRAINT `userrecommend_ibfk_2` FOREIGN KEY (`userId`) REFERENCES `user` (`userId`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
