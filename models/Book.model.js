6bead2e58779a034628d7bf0a11b1f2bc450a078
Manjiri Vadujkar <manjiri.vadujkar@gmail.com>
2021-02-26 13:42:02 +0530

    Sort books by book id

--- models/Book.model.js
+++ models/Book.model.js
@@ -8,7 +8,11 @@ async function getAll() {
   try {
     const query = `SELECT book.*, GROUP_CONCAT(bookchpt.chptname) as chapters from book INNER JOIN bookchpt ON book.bookId = bookchpt.bookId GROUP BY book.bookId`;
     const books = await getPromiseQuery()(query);
-    books.forEach(b => b.chapters = b.chapters.split(','));
+    books.forEach(b => {
+      b.chapters = b.chapters.split(',');
+      b.bookId = parseInt(b.bookId);
+    });
+    books.sort((a, b) => a.bookId - b.bookId);
     return books;
   } catch (e) {
     throw e;
